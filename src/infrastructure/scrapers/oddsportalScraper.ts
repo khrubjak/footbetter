da008731c0f01f2309ad9587b8eb13b62cab5b60
Karol Hrubjak <hrubjak.karol@gmail.com>
2026-05-30 11:42:43 +0200

    feat/multi-season-scraping

--- src/infrastructure/scrapers/oddsportalScraper.ts
+++ src/infrastructure/scrapers/oddsportalScraper.ts
@@ -1,95 +1,168 @@
-import { chromium } from "playwright";
+import { chromium } from 'playwright'
+import fs from 'fs'
 
-type MatchRow = {
-  date: string;
-  homeTeam: string;
-  awayTeam: string;
-  homeGoals: number;
-  awayGoals: number;
-  odds1: number | null;
-  oddsX: number | null;
-  odds2: number | null;
-};
+const outputFilePath = '../../../data/'
+const startYear = 2021
+const endYear = 2021
 
-const BASE =
-  "https://www.oddsportal.com/football/england/premier-league-2024-2025/results/";
-
-const PAGES = 7;
-
-async function scrapeSeason() {
-  const browser = await chromium.launch({ headless: true });
-  const page = await browser.newPage();
-
-  const allMatches: MatchRow[] = [];
-
-  for (let i = 1; i <= PAGES; i++) {
-    const url = i === 1 ? BASE : `${BASE}#/page/${i}/`; //#/page/7/
-
-    console.log(`Scraping page ${i}: ${url}`);
-
-    //await page.goto(url, { waitUntil: "networkidle" });
-
-    //await page.waitForSelector("min-h-[80vh]");
-  
-    await page.goto(url, {
-      waitUntil: "domcontentloaded"
-    });
-
-    await page.waitForLoadState("networkidle");
-
-    await page.waitForFunction(() => {
-      return document.querySelectorAll("a[href*='/football/']").length > 50;
-    });
-
-    const pageData = await page.$$eval("table tbody tr", rows => {
-      const results: MatchRow[] = [];
-
-      for (const row of rows) {
-        const cols = row.querySelectorAll("td");
-        if (cols.length < 5) continue;
-
-        const date = cols[0]?.textContent?.trim() || "";
-        const match = cols[1]?.textContent?.trim() || "";
-        const score = cols[2]?.textContent?.trim() || "";
-
-        // odds columns (typical layout)
-        const odds1 = parseFloat(cols[3]?.textContent || "");
-        const oddsX = parseFloat(cols[4]?.textContent || "");
-        const odds2 = parseFloat(cols[5]?.textContent || "");
-
-        const [homeTeam, awayTeam] = match.split(" - ");
-
-        const [homeGoals, awayGoals] = score.includes(":")
-          ? score.split(":").map(n => parseInt(n, 10))
-          : [NaN, NaN];
-
-        results.push({
-          date,
-          homeTeam: typeof homeTeam === "undefined" ? "" : homeTeam.trim(),
-          awayTeam: typeof awayTeam === "undefined" ? "" : awayTeam.trim(),
-          homeGoals: typeof homeGoals === "undefined" ? -1 : homeGoals,
-          awayGoals: typeof awayGoals === "undefined" ? -1 : awayGoals,
-          odds1: isNaN(odds1) ? null : odds1,
-          oddsX: isNaN(oddsX) ? null : oddsX,
-          odds2: isNaN(odds2) ? null : odds2,
-        });
-      }
-
-      return results;
-    });
-
-    allMatches.push(...pageData);
-
-    // small delay to avoid blocking
-    await page.waitForTimeout(2000);
-  }
-
-  await browser.close();
-
-  console.log(`Total matches scraped: ${allMatches.length}`);
-  console.log(JSON.stringify(allMatches, null, 2));
-
-  return allMatches;
+type sourceUrlParts = {
+  domain: string
+  sport: 'football'
+  country: 'england'
+  competition: string
+  //season: string
+  view: 'results' | 'fixtures' | 'standings' | 'odds'
+  expectedPages: number
 }
 
-scrapeSeason();
+type Match = {
+  //date: string
+  status: string
+  homeTeam: string
+  awayTeam: string
+  homeScore: number | null
+  awayScore: number | null
+  odds: {
+    home: number | null
+    draw: number | null
+    away: number | null
+  }
+}
+
+const source: sourceUrlParts = {
+  domain: 'https://www.oddsportal.com',
+  sport: 'football',
+  country: 'england',
+  competition: 'premier-league',
+  view: 'results',
+  expectedPages: 8,
+}
+
+const scrapedSeasonsArray = Array.from(
+  { length: endYear - startYear + 1 },
+  (_, i) => startYear + i,
+)
+
+async function scrapeSeason(year: number) {
+  const browser = await chromium.launch({ headless: true })
+  const page = await browser.newPage()
+
+  const allMatches: Match[] = []
+  const season = `${year}-` + `${year + 1}`
+
+  const BASE =
+    `${source.domain}/${source.sport}/` +
+    `${source.country}/` +
+    `${source.competition}-` +
+    `${season}/` +
+    `${source.view}/`
+
+  for (let i = 1; i <= source.expectedPages; i++) {
+    const url = i === 1 ? BASE : `${BASE}#/page/${i}/` //#/page/7/
+
+    console.log(`Scraping page ${i}: ${url}`)
+
+    await page.goto(url, {
+      waitUntil: 'networkidle',
+    })
+
+    // cookie popup
+    try {
+      await page
+        .getByRole('button', { name: /accept/i })
+        .click({ timeout: 3000 })
+    } catch {}
+
+    //await page.click("a[rel='next']")
+
+    await page.waitForSelector('[data-testid="game-row"]')
+
+    const matches: Match[] = await page.evaluate(() => {
+      const rows = document.querySelectorAll('[data-testid="game-row"]')
+
+      const results: Match[] = []
+
+      rows.forEach((row) => {
+        try {
+          // status
+          const status =
+            row
+              .querySelector('[data-testid="time-item"]')
+              ?.textContent?.trim() || ''
+
+          // teams
+          const teamElements = row.querySelectorAll(
+            '[data-testid="event-participants"] a[title]',
+          )
+
+          const homeTeam = teamElements[0]?.getAttribute('title')?.trim() || ''
+          const awayTeam = teamElements[1]?.getAttribute('title')?.trim() || ''
+
+          // scores
+          const scoreNodes = row.querySelectorAll(
+            '[data-testid="event-participants"] .relative .font-bold > div',
+          )
+
+          const scores = Array.from(scoreNodes)
+            .map((el) => parseInt(el.textContent?.trim() || '', 10))
+            .filter((n) => !isNaN(n))
+
+          const homeScore = scores[0] ?? null
+          const awayScore = scores[1] ?? null
+
+          // odds
+          const oddsEls = row.querySelectorAll(
+            '[data-testid="odd-container-default"], [data-testid="odd-container-winning"]',
+          )
+
+          const oddsValues = Array.from(oddsEls)
+            .map((el) => parseFloat(el.textContent || ''))
+            .filter((n) => !isNaN(n))
+
+          const odds = {
+            home: oddsValues[0] ?? null,
+            draw: oddsValues[2] ?? null,
+            away: oddsValues[4] ?? null,
+          }
+
+          if (odds.home !== null && odds.draw !== null && odds.away !== null) {
+            results.push({
+              status,
+              homeTeam,
+              awayTeam,
+              homeScore,
+              awayScore,
+              odds,
+            })
+          }
+        } catch {
+          console.log('Broken row detected when scraping')
+        }
+      })
+      return results
+    })
+
+    allMatches.push(...matches)
+
+    // small delay to avoid blocking
+    await page.waitForTimeout(2000)
+  }
+
+  await browser.close()
+
+  fs.writeFileSync(
+    outputFilePath +
+      [season, source.country, source.competition].join('_') +
+      '.json',
+    JSON.stringify(allMatches, null, 2),
+  )
+
+  console.log(
+    `Scraped ${allMatches.length} ${source.sport} matches from season ${season} ` +
+      `of ${source.competition} in ${source.country}`,
+  )
+}
+
+for (let year = startYear; year <= endYear; year++) {
+  scrapeSeason(year)
+}
